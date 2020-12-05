639e3f53a811028974ddc8937acabfb18a59dd34
Shannon Sagehorn <srsagehorn@gmail.com>
2020-12-05 00:52:21 -0600

    validated

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 // all of the buttons on the page and whether or not they are collapsed
 
 let features = [
-  { name: "forloop", collapsed: true },
-  { name: "fruits", collapsed: true },
-  { name: "days", collapsed: true },
-  { name: "nums", collapsed: true },
+  { name: "carsJS", collapsed: true },
+  { name: "carsClass", collapsed: true },
+  {name : "fam", collapsed: true},
+  {name : "animals", collapsed: true}
 ];
 
 // tutorial in motion!!
@@ -25,3 +25,19 @@ features.forEach((button) => {
     }
   });
 });
+
+
+class FamilyMembers {
+  constructors(name, age, relation) {
+        this.name = name;
+        this.age = age;
+        this.relation = relation;
+          }
+        }
+
+let auntBetty = new FamilyMembers("Betty", 57, "aunt")
+let uncleRoger = new FamilyMembers("Roger", 58, "Uncle")
+let cousinJen = new FamilyMembers("Jen", 23, "Cousin")
+let stepmomJulie = new FamilyMembers("Julie", 46, "Step Mom")
+
+console.log (auntBetty, uncleRoger, cousinJen, stepmomJulie)
